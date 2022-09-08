6e6756da12e5878a22692e220669a3259f1f1831
baalmeida <bj.aa.414@gmail.com>
2022-09-08 13:34:55 -0500

    Added jump and gravity

--- PlayerDino.gd
+++ PlayerDino.gd
@@ -2,11 +2,15 @@ extends RigidBody2D
 
 onready var anim_tree := $AnimationTree
 export var speed := 50.0
-export var jump_speed := 100.0
+export var jump_speed := 1200.0
+export var gravity := 10.0
+var gravity_speed := 0.0
  
+var is_grounded := true
+
 func _process(delta):
     var input_vector = Vector2.ZERO
-   
+    
     input_vector.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * speed
 
     if input_vector == Vector2.ZERO:
@@ -18,14 +22,26 @@ func _process(delta):
         anim_tree.set("parameters/Run/blend_position", input_vector.x)
         anim_tree.get("parameters/playback").travel("Run")
       
-        apply_central_impulse(input_vector)
         
-    if Input.is_action_just_pressed("ui_up"):
+    if Input.is_action_just_pressed("ui_up") and is_grounded:
+        gravity_speed = 0.0
+        apply_central_impulse(Vector2.UP *jump_speed)
         anim_tree.get("parameters/playback").travel("Headbutt")
+    elif not is_grounded:
+
+        gravity_speed += gravity + delta
+        apply_central_impulse(Vector2.DOWN*gravity_speed)
+        
+          
+        
     if Input.is_action_just_pressed("ui_down"):
         anim_tree.get("parameters/playback").travel("Kick")
-        
-   # if Input.is_action_pressed("ui_down"):    
     
+    apply_impulse(Vector2(),input_vector)
+        
+  
+
+func _integrate_forces(state):
+    is_grounded = state.get_contact_count() > 0 and int(state.get_contact_collider_position(0).y) >= int(global_position.y)
 
 
