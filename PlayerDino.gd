40a5c6a2332ee34e282fa0d5881654e26754c82f
baalmeida <bj.aa.414@gmail.com>
2022-09-08 11:54:02 -0500

    Added kick and headbutt animations

--- PlayerDino.gd
+++ PlayerDino.gd
@@ -1,18 +1,31 @@
 extends RigidBody2D
 
 onready var anim_tree := $AnimationTree
-
+export var speed := 50.0
+export var jump_speed := 100.0
+ 
 func _process(delta):
     var input_vector = Vector2.ZERO
    
-    input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-    input_vector.y = Input.get_action_strength("ui_up")
-    input_vector = input_vector.normalized()
-    
+    input_vector.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * speed
+
     if input_vector == Vector2.ZERO:
-        $AnimationTree.get("parameters/playback").travel("Idle")
+        anim_tree.get("parameters/playback").travel("Idle")
     else:
-        $AnimationTree.get("parameters/playback").travel("Run")
-        #move_and_slide(input_vector * speed)
-        $AnimationTree.set("parameters/Idle/blend_position", input_vector.x)
-        $AnimationTree.set("parameters/Run/blend_position", input_vector.x)
+        anim_tree.set("parameters/Kick/blend_position", input_vector.x)
+        anim_tree.set("parameters/Headbutt/blend_position", input_vector.x) 
+        anim_tree.set("parameters/Idle/blend_position", input_vector.x)
+        anim_tree.set("parameters/Run/blend_position", input_vector.x)
+        anim_tree.get("parameters/playback").travel("Run")
+      
+        apply_central_impulse(input_vector)
+        
+    if Input.is_action_just_pressed("ui_up"):
+        anim_tree.get("parameters/playback").travel("Headbutt")
+    if Input.is_action_just_pressed("ui_down"):
+        anim_tree.get("parameters/playback").travel("Kick")
+        
+   # if Input.is_action_pressed("ui_down"):    
+    
+
+
